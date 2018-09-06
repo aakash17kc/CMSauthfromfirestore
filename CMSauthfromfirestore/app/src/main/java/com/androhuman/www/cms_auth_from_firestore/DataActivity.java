300c3bc022b4bd21873358969548073958fe132e
aakash17kc <aakash17.kc@gmail.com>
2018-09-06 23:17:28 +0530

    json

---
+++ CMSauthfromfirestore/app/src/main/java/com/androhuman/www/cms_auth_from_firestore/DataActivity.java
@@ -0,0 +1,13 @@
+package com.androhuman.www.cms_auth_from_firestore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+    }
+}
